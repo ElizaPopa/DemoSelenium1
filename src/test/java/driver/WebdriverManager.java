81b68ddd4560f8003de3783ae7f039c803fc1b18
ElizaPopa <oprea.eliza7@gmail.com>
2022-12-22 15:14:39 +0200

    Project updated

--- src/test/java/driver/WebdriverManager.java
+++ src/test/java/driver/WebdriverManager.java
@@ -3,8 +3,15 @@ package driver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebdriverManager {
+
     public static ChromeDriver createChromeDriver() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         return new ChromeDriver();
+    }
+
+    public static ChromeDriver createDriverAndGetPage() {
+        ChromeDriver driver = WebdriverManager.createChromeDriver();
+        driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
+        return driver;
     }
 }
